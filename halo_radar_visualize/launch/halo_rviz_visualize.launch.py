2e8ef72f31d20591be43044e219162938467f962
ChrisYang <chris.yang@jetseaai.com>
2024-12-02 17:27:08 +0800

    fix(launch): update RViz config file extension
    Changed the RViz configuration file extension from .yaml to .rviz in the
    halo_rviz_visualize.launch.py script to ensure proper loading of the
    visualization settings.

--- halo_radar_visualize/launch/halo_rviz_visualize.launch.py
+++ halo_radar_visualize/launch/halo_rviz_visualize.launch.py
@@ -7,7 +7,7 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
 
     rviz2_config = os.path.join(
-        get_package_share_directory("halo_radar_visualize"), "rviz2", "visualize.yaml"
+        get_package_share_directory("halo_radar_visualize"), "rviz2", "visualize.rviz"
     )
     return LaunchDescription(
         [
